cd3c6c955edb2224cdba77d21758f5d9a74026f8
Ttst <thibautstachnick@gmail.com>
2023-12-28 11:52:53 +0100

    add a function that simulate a heavy calc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    simulate_heavy_calc(40);
+}
+
+
+fn simulate_heavy_calc(intensity: u64)->u64{
+    println!("Heavy calculating...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }
