131fed70705fa2baf5aaaca97a41b4585f2544ab
Ttst <thibautstachnick@gmail.com>
2023-12-28 12:07:29 +0100

    add a function that simulate an exercise generator

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,27 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    simulate_heavy_calc(40);
+    let simulated_user_value = 10;
+    let simulated_random_number = 6;
+
+    generate_exercises(simulated_user_value, simulated_random_number)
+
+}
+
+fn generate_exercises(intensity :u64, random_number :u64) {
+    if intensity < 25 {
+        println!("Today make {} pushups",
+                 simulate_heavy_calc(intensity));
+        println!("then, do {} abs",
+            simulate_heavy_calc(intensity));
+    }else {
+        if random_number == 3 {
+            println!("It's your day off today, stay hydrated");
+        }else {
+            println!("Today, you have to run {} minutes",
+                simulate_heavy_calc(intensity));
+        }
+    }
 }
 
 
