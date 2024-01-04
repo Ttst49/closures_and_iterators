df03951287cd123172ff05d87a9880a876027a0d
Ttst <thibautstachnick@gmail.com>
2024-01-04 22:25:54 +0100

    add an example of closure that make an error for learning purpose

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,32 @@ fn main() {
 }
 
 fn generate_exercises(intensity :u64, random_number :u64) {
-    let light_result = simulate_heavy_calc(intensity);
+    let heavy_calc = |intensity:u64| ->u64{
+        println!("Heavy calculating...");
+        thread::sleep(Duration::from_secs(2));
+        intensity
+    };
 
     if intensity < 25 {
-        println!("Today make {} pushups",light_result);
-        println!("then, do {} abs",light_result);
+        println!("Today make {} pushups",heavy_calc(intensity));
+        println!("then, do {} abs",heavy_calc(intensity));
     }else {
         if random_number == 3 {
             println!("It's your day off today, stay hydrated");
         }else {
-            println!("Today, you have to run {} minutes",light_result);
+            println!("Today, you have to run {} minutes",heavy_calc(intensity));
         }
     }
+
+    //Example of closure, you can only have one return type per closure
+    //this result into an error in the compiler
+    /*
+    let test_closure = |x| x;
+
+    test_closure(12);
+    test_closure("12");
+     */
+
 }
 
 
-fn simulate_heavy_calc(intensity: u64)->u64{
-    println!("Heavy calculating...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
